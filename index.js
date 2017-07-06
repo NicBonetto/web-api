184f5ab82cad598b30b5d4bf550e0869f007ee05
Nic Bonetto <nick.bonetto@gmail.com>
2017-07-06 10:35:02 -0700

    Revised notes id to originate from server. #3

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const app = express()
 const bodyParser = require('body-parser')
 
 const notes = []
+let notesIndex = 0
 
 app.use(bodyParser.json())
 
 app.post('/notes', (req, res) => {
   notes.push(req.body)
+  notes[notesIndex].id = notesIndex + 1
   res.sendStatus(201)
+  notesIndex++
 })
 
 app.get('/notes', (req, res) => {
@@ -17,8 +20,11 @@ app.get('/notes', (req, res) => {
 
 app.put('/notes/:id', (req, res) => {
   notes.forEach((element, index) => {
+    let saveId
     if (element.id === Number(req.params.id)) {
+      saveId = element.id
       notes.splice(index, 1, req.body)
+      notes[index].id = saveId
     }
   })
   res.sendStatus(200)
